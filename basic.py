699edde0c69f210416c589830988cf95dd924293
Ujjalzaman <muzzwal97@gmail.com>
2024-04-29 14:54:46 +0600

    String method practice

---
+++ basic.py
@@ -0,0 +1,78 @@
+# Syntax
+# print("hellow python") 
+
+
+# if 5 > 2:
+    # print("five is greater then 2")
+
+
+# str, int, float 
+
+# python has 8 kinds of data types
+
+# text type - string 
+# numeric type - int, float, complext
+# sequence type - tuple, list, range
+# set type - set
+# boolean type - true false, bool
+# maping type - dict
+# none type - none
+# binary type - binary, binarmaping
+
+# String====================================
+myVar = 'hellow world'
+# print(myVar[2:5])
+# print(myVar[:2])
+# print(myVar.upper())
+
+# print(myVar.strip())
+# modify - replace, stripe,split,upper, lower
+# concatenation - a + b
+# escape charachter - \ \\ \n 
+
+# string method
+# capialize()
+# casefold - convert string into lower case
+# center - returns a centered string
+# count();
+# encode(): 
+# endswith()
+# expandtabs():
+# find():
+# format():
+# format_map():
+# index();
+# isdigit();
+# islower();
+# isspace();
+# istitle();
+# issupper();
+# isstrip();
+# partition():
+# replace():
+# rfind();
+# rindex():
+# startwith();
+# swapcase():
+# title():
+# translate():
+# upper();
+
+
+
+
+
+
+
+
+# Numerice Data type
+# myNum = 1243
+# y= 34.5
+
+# # sequnce data type
+# # tuple - unchangeable ,immutable
+# tupeVar = ("apple", "manuge", "foo")
+# # list - changeable , mutable
+# listVar = ['foor', 'baar', 'heel']
+
+
