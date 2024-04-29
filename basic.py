856151570079fda79066394803250952d915d628
Ujjalzaman <muzzwal97@gmail.com>
2024-04-29 16:21:03 +0600

    Add basic type of python

--- basic.py
+++ basic.py
@@ -20,7 +20,7 @@
 # binary type - binary, binarmaping
 
 # String====================================
-myVar = 'hellow world'
+# myVar = 'hellow world'
 # print(myVar[2:5])
 # print(myVar[:2])
 # print(myVar.upper())
@@ -59,20 +59,157 @@ myVar = 'hellow world'
 # upper();
 
 
+# Boolean Methods
+# > == <
+# Operators
+# +, -, *, /,%, **, //
 
 
+# List =====================
+# a = ['apple', 'banana', 'cherry', 'apple', 'something else']
+# a[1]
+# a[1:5]
+# a[:5]
+# a[2:]
+
+# if 'apple' in a:
+#     print('yes apple has')
+
+# # change items 
+# a[1] = 'katol'
+# a.append('orange')
+# a.append(2,'komola')
 
 
+# x = [1,4,5,5,67]
+# y = (4,6,6,6,7,8)
+# x.extend(y)
+# print(x)
+
+# # To remove list items
+# remove,
+# pop() - remove last items ('items')
+# del listname[4]
+# del listname
+# a.clear();
+
+# Loop throug in Lists ==========================
+# a = [3,5,6,67,7,8,8,99]
+# for x in a:
+#     print(a)
 
 
-# Numerice Data type
-# myNum = 1243
-# y= 34.5
-
-# # sequnce data type
-# # tuple - unchangeable ,immutable
-# tupeVar = ("apple", "manuge", "foo")
-# # list - changeable , mutable
-# listVar = ['foor', 'baar', 'heel']
+# for x in range(len(x)):
+#     print(a)
 
 
+# # copy lists
+# a.copy();
+# mylsist = list(a);
+
+# b = [4,6778,9,000,33]
+# a.extend(b)
+# for i in b:
+#     a.append(i)
+
+# List methods =============================
+
+# append();
+# clear();
+# copy();
+# count();
+# extend();
+# index();
+# insert();
+# pop();
+# remove();
+# reverse();
+# sort();
+
+# tuple = unchangable (immutable )
+# it's also access item by index
+
+# to update tuple
+# x = ("hellow", "world")
+# (a,b) = x
+# print(a,b)
+# y = list(x);
+# y[1] = 'galaxy'
+
+# print(y)
+# append 
+# can be loop as well
+# can be tuple multpley
+
+# Sets = {}
+# mysets = {"apple", "microsoft", "someth"}
+# unchangalbe but we can remove or add new items duplicatte now allowed
+
+# Python dictionary 
+# thisdic = {
+#     "brand":"apple"
+# }
+
+# # Dictionary Methods =======================
+# clear();
+# copy();
+# formkeys();
+# get();
+# items();
+# keys();
+# pop();
+# poptiem();
+# setdefault();
+# update();
+# values();
+
+# Python condition 
+# a == b
+# !=
+# <
+# >
+# <=
+# >=
+# if b > a :
+#     print("hellow")
+# else :
+#     print("hellow wot")
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+
+# Loop ================================
+# for loop
+# for x in a:
+#     print (x)
+
+
+# loop can be possible in string
+
+# for x in "banner":
+#     print (x)
+# function =========================================
+# def myfunction():
+#     print("hellow ")
+
+# class ========================
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+
+# p = Person('ujjal', 30)
+# print(p.name +' ' + str(p.age))
+
+# Python modules
+# NumPy
+# Pandas
+# Scipy
+# django
